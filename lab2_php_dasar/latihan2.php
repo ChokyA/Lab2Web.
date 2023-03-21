5bec3f4b902e545fb9b8e1f383ad4addc83a46da
Choky <93463612+ChokyA@users.noreply.github.com>
2023-03-21 13:03:53 +0700

    Add files via upload

---
+++ lab2_php_dasar/latihan2.php
@@ -0,0 +1,6 @@
+<body>
+    <h1>Predifine Variable</h1>
+    <?php
+    echo 'Selamat Datang ' . $_GET['nama'];
+    ?>
+</body>
---
+++ lab2_php_dasar/php_dasar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World";
+    ?>
+</body>
+
+<body>
+    <h1>Menggunakan Variabel</h1>
+    <?php
+    $nim = "312110322";
+    $nama = 'Choky Andriano';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+</body>
+
+</html>
